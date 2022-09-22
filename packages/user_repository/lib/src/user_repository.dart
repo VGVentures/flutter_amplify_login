d0b34bdeb05c645a8f670c9958023e43376caa8e
Ana Polo <anna-91_@hotmail.com>
2022-09-22 10:54:43 +0200

    feat: add user repository

---
+++ packages/user_repository/lib/src/user_repository.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// {@template user_repository}
+/// A package which manages the user domain
+/// {@endtemplate}
+class UserRepository {
+  /// {@macro user_repository}
+  const UserRepository();
+}
