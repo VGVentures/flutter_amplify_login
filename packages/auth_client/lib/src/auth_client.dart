1751b6ab804e8111e07ee713b731789832b8f495
Ana Polo <anna-91_@hotmail.com>
2022-09-22 13:01:07 +0200

    feat: add auth methods

--- packages/auth_client/lib/src/auth_client.dart
+++ packages/auth_client/lib/src/auth_client.dart
@@ -5,10 +5,118 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+
+/// {@template auth_exception}
+/// Abstract class to handle the amplify auth exceptions.
+/// {@endtemplate}
+abstract class AuthException implements Exception {
+  /// {@macro auth_exception}
+  const AuthException(this.error);
+
+  /// The error which was caught.
+  final Object error;
+}
+
+/// {@template sign_up_failure}
+/// Thrown during the sign up process if a failure occurs.
+/// {@endtemplate}
+class SignUpFailure extends AuthException {
+  /// {@macro sign_up_failure}
+  const SignUpFailure(super.error);
+}
+
+/// {@template confirmarion_code_sign_up_failure}
+/// Thrown during the verification of the confiramtion code
+/// sign up process if a failure occurs.
+/// {@endtemplate}
+class ConfirmationCodeSignUpFailure extends AuthException {
+  /// {@macro confirmarion_code_sign_up_failure}
+  const ConfirmationCodeSignUpFailure(super.error);
+}
+
+/// {@template sign_in_failure}
+/// Thrown during the sign in process if a failure occurs.
+/// {@endtemplate}
+class SignInFailure extends AuthException {
+  /// {@macro sign_in_failure}
+  const SignInFailure(super.error);
+}
+
+/// {@template sign_out_failure}
+/// Thrown during the sign out process if a failure occurs.
+/// {@endtemplate}
+class SignOutFailure extends AuthException {
+  /// {@macro sign_out_failure}
+  const SignOutFailure(super.error);
+}
+
 /// {@template auth_client}
 /// AWS Amplify auth client
 /// {@endtemplate}
 class AuthClient {
   /// {@macro auth_client}
-  const AuthClient();
+  AuthClient({
+    required AuthCategory auth,
+  }) : _auth = auth;
+
+  final AuthCategory _auth;
+
+  /// Creates a new user with the [email] and [password] variables.
+  Future<void> signUp(
+    String email,
+    String password,
+  ) async {
+    try {
+      final userAttributes = <CognitoUserAttributeKey, String>{
+        CognitoUserAttributeKey.email: email,
+      };
+      final options = CognitoSignUpOptions(userAttributes: userAttributes);
+
+      await _auth.signUp(
+        username: email,
+        password: password,
+        options: options,
+      );
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(SignUpFailure(error), stackTrace);
+    }
+  }
+
+  /// Confirm the sign up with a confirmation code.
+  Future<void> confirmSignUp(String email, String confirmationCode) async {
+    try {
+      await _auth.confirmSignUp(
+        username: email,
+        confirmationCode: confirmationCode,
+      );
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(
+        ConfirmationCodeSignUpFailure(error),
+        stackTrace,
+      );
+    }
+  }
+
+  /// Sign in with the provided [email] and [password].
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _auth.signIn(
+        username: email,
+        password: password,
+      );
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(SignInFailure(error), stackTrace);
+    }
+  }
+
+  /// Sign out the current user.
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(SignOutFailure(error), stackTrace);
+    }
+  }
 }
--- packages/auth_client/test/src/auth_client_test.dart
+++ packages/auth_client/test/src/auth_client_test.dart
@@ -5,15 +5,152 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:auth_client/auth_client.dart';
 // ignore_for_file: prefer_const_constructors
 
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:auth_client/auth_client.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class _MockAmplifyAuth extends Mock implements AuthCategory {}
+
+class _FakeSignInResult extends Fake implements SignInResult {}
+
+class _FakeSignUpResult extends Fake implements SignUpResult {}
+
+class _FakeSignOutResult extends Fake implements SignOutResult {}
 
 void main() {
+  late AuthClient authClient;
+  late AuthCategory auth;
+  late SignInResult signInResultResponse;
+  late SignUpResult signUpResultResponse;
+  late SignOutResult signOutResultResponse;
+
+  const email = 'test@test.com';
+  const password = 'Password';
+
+  setUp(() {
+    auth = _MockAmplifyAuth();
+    signInResultResponse = _FakeSignInResult();
+    signUpResultResponse = _FakeSignUpResult();
+    signOutResultResponse = _FakeSignOutResult();
+    authClient = AuthClient(auth: auth);
+  });
   group('AuthClient', () {
     test('can be instantiated', () {
-      expect(AuthClient(), isNotNull);
+      expect(AuthClient(auth: auth), isNotNull);
+    });
+
+    group('SignIn', () {
+      test('completes', () async {
+        when(
+          () => auth.signIn(
+            username: any(named: 'username'),
+            password: any(named: 'password'),
+          ),
+        ).thenAnswer(
+          (_) async => signInResultResponse,
+        );
+
+        expect(authClient.signIn(email, password), completes);
+      });
+
+      test('throw SignInFailure', () async {
+        when(
+          () => auth.signIn(
+            username: any(named: 'username'),
+            password: any(named: 'password'),
+          ),
+        ).thenThrow((_) async => Exception());
+
+        expect(
+          authClient.signIn(email, password),
+          throwsA(isA<SignInFailure>()),
+        );
+      });
+    });
+
+    group('SignUp', () {
+      test('completes', () async {
+        when(
+          () => auth.signUp(
+            username: any(named: 'username'),
+            password: any(named: 'password'),
+            options: any(named: 'options'),
+          ),
+        ).thenAnswer(
+          (_) async => signUpResultResponse,
+        );
+
+        expect(authClient.signUp(email, password), completes);
+      });
+
+      test('throw SignUpFailure', () async {
+        when(
+          () => auth.signIn(
+            username: any(named: 'username'),
+            password: any(named: 'password'),
+          ),
+        ).thenThrow((_) async => Exception());
+
+        expect(
+          authClient.signUp(email, password),
+          throwsA(isA<SignUpFailure>()),
+        );
+      });
+    });
+
+    group('SignOut', () {
+      test('completes', () async {
+        when(
+          () => auth.signOut(),
+        ).thenAnswer(
+          (_) async => signOutResultResponse,
+        );
+
+        expect(authClient.signOut(), completes);
+      });
+
+      test('throw SignOutFailure', () async {
+        when(
+          () => auth.signOut(),
+        ).thenThrow((_) async => Exception());
+
+        expect(
+          authClient.signOut(),
+          throwsA(isA<SignOutFailure>()),
+        );
+      });
+    });
+
+    group('ConfirmSignUp', () {
+      test('completes', () async {
+        when(
+          () => auth.confirmSignUp(
+            username: any(named: 'username'),
+            confirmationCode: any(named: 'confirmationCode'),
+          ),
+        ).thenAnswer(
+          (_) async => signUpResultResponse,
+        );
+
+        expect(authClient.confirmSignUp(email, password), completes);
+      });
+
+      test('throw ConfirmationCodeSignUpFailure', () async {
+        when(
+          () => auth.confirmSignUp(
+            username: any(named: 'username'),
+            confirmationCode: any(named: 'confirmationCode'),
+          ),
+        ).thenThrow((_) async => Exception());
+
+        expect(
+          authClient.confirmSignUp(email, password),
+          throwsA(isA<ConfirmationCodeSignUpFailure>()),
+        );
+      });
     });
   });
 }
