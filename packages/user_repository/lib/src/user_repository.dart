fa3f480a7da67765115b9636b6ac0e58e6105e51
Ana Polo <anna-91_@hotmail.com>
2022-10-11 13:20:04 +0200

    feat: add user repository implementation

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -17,4 +17,74 @@ class UserRepository {
   }) : _authClient = authClient;
 
   final AuthClient _authClient;
+
+  /// Stream of [AmplifyUser] which will emit the current user when
+  /// the authentication state changes.
+  ///
+  /// Emits [AmplifyUser.anonymous] if the user is not authenticated.
+  Stream<AmplifyUser> get user => _authClient.user;
+
+  /// Starts the Sign Up Flow.
+  ///
+  /// Throws a [SignUpFailure] if an exception occurs.
+  Future<void> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _authClient.signUp(email, password);
+    } on SignUpFailure {
+      rethrow;
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(SignUpFailure(error), stackTrace);
+    }
+  }
+
+  /// Starts the Sign In Flow.
+  ///
+  /// Throws a [SignInFailure] if an exception occurs.
+  Future<void> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _authClient.signIn(email, password);
+    } on SignInFailure {
+      rethrow;
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(SignInFailure(error), stackTrace);
+    }
+  }
+
+  /// Starts the Sign Out Flow.
+  ///
+  /// Throws a [SignOutFailure] if an exception occurs.
+  Future<void> signOut() async {
+    try {
+      await _authClient.signOut();
+    } on SignOutFailure {
+      rethrow;
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(SignOutFailure(error), stackTrace);
+    }
+  }
+
+  /// Starts the Confirmation Sign Up Flow.
+  ///
+  /// Throws a [ConfirmationCodeSignUpFailure] if an exception occurs.
+  Future<void> confirmSignUp({
+    required String email,
+    required String confirmationCode,
+  }) async {
+    try {
+      await _authClient.confirmSignUp(email, confirmationCode);
+    } on ConfirmationCodeSignUpFailure {
+      rethrow;
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(
+        ConfirmationCodeSignUpFailure(error),
+        stackTrace,
+      );
+    }
+  }
 }
--- packages/user_repository/test/src/user_repository_test.dart
+++ packages/user_repository/test/src/user_repository_test.dart
@@ -5,8 +5,6 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-// ignore_for_file: prefer_const_constructors
-
 import 'package:auth_client/auth_client.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -14,12 +12,32 @@ import 'package:user_repository/user_repository.dart';
 
 class _MockAuthClient extends Mock implements AuthClient {}
 
+class FakeSignInFailure extends Fake implements SignInFailure {}
+
+class FakeSignOutFailure extends Fake implements SignOutFailure {}
+
+class FakeSignUpFailure extends Fake implements SignUpFailure {}
+
+class FakeConfirmationCodeSignUpFailure extends Fake
+    implements ConfirmationCodeSignUpFailure {}
+
+class MockUser extends Mock implements AmplifyUser {}
+
+const emailTest = 'emal@test.com';
+const passwordTest = '1234Test';
+const confirmationCodeTest = '123456789';
+
 void main() {
   late AuthClient authClient;
+  late UserRepository userRepository;
 
   setUp(() {
     authClient = _MockAuthClient();
+    userRepository = UserRepository(
+      authClient: authClient,
+    );
   });
+
   group('UserRepository', () {
     test('can be instantiated', () {
       expect(
@@ -28,6 +46,155 @@ void main() {
         ),
         isNotNull,
       );
+    });
+
+    group('user', () {
+      test('calls user on AuthClient', () {
+        when(() => authClient.user).thenAnswer(
+          (_) => const Stream.empty(),
+        );
+        userRepository.user;
+        verify(() => authClient.user).called(1);
+      });
+    });
+
+    group('signIn', () {
+      test('calls signIn on AuthClient', () async {
+        when(
+          () => authClient.signIn(emailTest, passwordTest),
+        ).thenAnswer((_) async {});
+        await userRepository.signIn(
+          email: emailTest,
+          password: passwordTest,
+        );
+        verify(() => authClient.signIn(emailTest, passwordTest)).called(1);
+      });
+
+      test('rethrows SignInFailure', () async {
+        final exception = FakeSignInFailure();
+        when(
+          () => authClient.signIn(emailTest, passwordTest),
+        ).thenThrow(exception);
+        expect(
+          () => userRepository.signIn(email: emailTest, password: passwordTest),
+          throwsA(exception),
+        );
+      });
+
+      test('throws SignInFailure on generic exception', () async {
+        when(
+          () => authClient.signIn(emailTest, passwordTest),
+        ).thenThrow(Exception());
+        expect(
+          () => userRepository.signIn(email: emailTest, password: passwordTest),
+          throwsA(isA<SignInFailure>()),
+        );
+      });
+    });
+
+    group('signUp', () {
+      test('calls signUp on AuthClient', () async {
+        when(
+          () => authClient.signUp(emailTest, passwordTest),
+        ).thenAnswer((_) async {});
+        await userRepository.signUp(
+          email: emailTest,
+          password: passwordTest,
+        );
+        verify(() => authClient.signUp(emailTest, passwordTest)).called(1);
+      });
+
+      test('rethrows SignUpFailure', () async {
+        final exception = FakeSignUpFailure();
+        when(
+          () => authClient.signUp(emailTest, passwordTest),
+        ).thenThrow(exception);
+        expect(
+          () => userRepository.signUp(email: emailTest, password: passwordTest),
+          throwsA(exception),
+        );
+      });
+
+      test('throws SignInFailure on generic exception', () async {
+        when(
+          () => authClient.signUp(emailTest, passwordTest),
+        ).thenThrow(Exception());
+        expect(
+          () => userRepository.signUp(email: emailTest, password: passwordTest),
+          throwsA(isA<SignUpFailure>()),
+        );
+      });
+    });
+
+    group('signOut', () {
+      test('calls signOut on AuthClient', () async {
+        when(
+          () => authClient.signOut(),
+        ).thenAnswer((_) async {});
+        await userRepository.signOut();
+        verify(() => authClient.signOut()).called(1);
+      });
+
+      test('rethrows SignOutFailure', () async {
+        final exception = FakeSignOutFailure();
+        when(() => authClient.signOut()).thenThrow(exception);
+        expect(
+          () => userRepository.signOut(),
+          throwsA(exception),
+        );
+      });
+
+      test('throws SignOutFailure on generic exception', () async {
+        when(
+          () => authClient.signOut(),
+        ).thenThrow(Exception());
+        expect(
+          () => userRepository.signOut(),
+          throwsA(isA<SignOutFailure>()),
+        );
+      });
+    });
+
+    group('confirmSignUp', () {
+      test('calls confirmSignUp on AuthClient', () async {
+        when(
+          () => authClient.confirmSignUp(emailTest, confirmationCodeTest),
+        ).thenAnswer((_) async {});
+        await userRepository.confirmSignUp(
+          email: emailTest,
+          confirmationCode: confirmationCodeTest,
+        );
+        verify(() => authClient.confirmSignUp(emailTest, confirmationCodeTest))
+            .called(1);
+      });
+
+      test('rethrows ConfirmationCodeSignUpFailure', () async {
+        final exception = FakeConfirmationCodeSignUpFailure();
+        when(
+          () => authClient.confirmSignUp(emailTest, confirmationCodeTest),
+        ).thenThrow(exception);
+        expect(
+          () => userRepository.confirmSignUp(
+            email: emailTest,
+            confirmationCode: confirmationCodeTest,
+          ),
+          throwsA(exception),
+        );
+      });
+
+      test('throws ConfirmationCodeSignUpFailure on generic exception',
+          () async {
+        when(
+          () => authClient.confirmSignUp(emailTest, confirmationCodeTest),
+        ).thenThrow(Exception());
+        expect(
+          () => userRepository.confirmSignUp(
+            email: emailTest,
+            confirmationCode: confirmationCodeTest,
+          ),
+          throwsA(isA<ConfirmationCodeSignUpFailure>()),
+        );
+      });
     });
   });
 }
