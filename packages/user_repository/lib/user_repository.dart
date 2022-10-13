28fc58177025f8190eda1792c3aa7814f04297cd
Ana Polo <anna-91_@hotmail.com>
2022-10-13 16:23:26 +0200

    feat: user repository (#3)
    * feat: add user repository
    
    * feat: update workflow
    
    * feat: update flutter versio

---
+++ packages/user_repository/lib/user_repository.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// A package which manages the user domain
+library user_repository;
+
+export 'src/user_repository.dart';
