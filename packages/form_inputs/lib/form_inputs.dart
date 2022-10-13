f4d6b7256c981014ecf9598aa0b6d2c629119fd7
Ana Polo <anna-91_@hotmail.com>
2022-10-13 11:06:11 +0200

    feat: create form input package

---
+++ packages/form_inputs/lib/form_inputs.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// A Dart package which exposes reusable form inputs and validation rules
+library form_inputs;
+
+export 'src/form_inputs.dart';
