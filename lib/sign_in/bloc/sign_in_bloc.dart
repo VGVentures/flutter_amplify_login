a80dc71b9e4f6b89839f993b87f39b40cd996c6f
Ana Polo <anna-91_@hotmail.com>
2022-10-14 10:46:10 +0200

    feat: add sign in bloc

---
+++ lib/sign_in/bloc/sign_in_bloc.dart
@@ -0,0 +1,64 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:form_inputs/form_inputs.dart';
+import 'package:user_repository/user_repository.dart';
+
+part 'sign_in_event.dart';
+part 'sign_in_state.dart';
+
+class SignUpBloc extends Bloc<SignInEvent, SignInState> {
+  SignUpBloc({
+    required UserRepository userRepository,
+  })  : _userRepository = userRepository,
+        super(const SignInState()) {
+    on<SignInEmailChanged>(_onEmailChanged);
+    on<SignInPasswordChanged>(_onPasswordChanged);
+    on<SignInSubmitted>(_onSignInSubmitted);
+  }
+
+  final UserRepository _userRepository;
+
+  void _onEmailChanged(
+    SignInEmailChanged event,
+    Emitter<SignInState> emit,
+  ) {
+    final email = Email.dirty(event.email);
+    emit(
+      state.copyWith(
+        email: email,
+        valid: Formz.validate([email, state.password]),
+      ),
+    );
+  }
+
+  void _onPasswordChanged(
+    SignInPasswordChanged event,
+    Emitter<SignInState> emit,
+  ) {
+    final password = Password.dirty(event.password);
+    emit(
+      state.copyWith(
+        password: password,
+        valid: Formz.validate([password, state.email]),
+      ),
+    );
+  }
+
+  Future<void> _onSignInSubmitted(
+    SignInSubmitted event,
+    Emitter<SignInState> emit,
+  ) async {
+    if (!state.valid) return;
+    emit(state.copyWith(status: SignInStatus.signInLoading));
+    try {
+      await _userRepository.signIn(
+        email: state.email.value,
+        password: state.password.value,
+      );
+      emit(state.copyWith(status: SignInStatus.signInSuccess));
+    } catch (error, stackTrace) {
+      emit(state.copyWith(status: SignInStatus.signInFailure));
+      addError(error, stackTrace);
+    }
+  }
+}
