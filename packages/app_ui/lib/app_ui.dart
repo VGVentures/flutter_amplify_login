4776fe40adce0dca925a06855bf9081d39b49a5e
Ana Polo <anna-91_@hotmail.com>
2022-10-28 13:02:57 +0200

    feat: add app ui package

---
+++ packages/app_ui/lib/app_ui.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// A package which manage the common widgets of the app
+library app_ui;
+
+export 'src/app_ui.dart';
---
+++ packages/app_ui/lib/src/app_ui.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// {@template app_ui}
+/// A package which manage the common widgets of the app
+/// {@endtemplate}
+class AppUi {
+  /// {@macro app_ui}
+  const AppUi();
+}
