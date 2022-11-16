289990a412aa828ec28cb3b0371ccb7466cef012
Ana Polo <anna-91_@hotmail.com>
2022-11-16 09:43:27 +0100

    chore: move main and bootstrap files to the root (#22)

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -8,7 +8,7 @@
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:auth_client/auth_client.dart';
 import 'package:flutter_amplify_login/app/app.dart';
-import 'package:flutter_amplify_login/main/bootstrap/bootstrap.dart';
+import 'package:flutter_amplify_login/bootstrap.dart';
 import 'package:user_repository/user_repository.dart';
 
 void main() {
